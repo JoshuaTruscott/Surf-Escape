45f4e826df72a14d2ad757d5aabd2ad51a1546a5
Joshua Truscott <joshuatruscott@Joshuas-MacBook-Pro.local>
2014-06-30 15:52:40 +1000

    Initial Commit

---
+++ Surf EscapeTests/Surf_EscapeTests.swift
@@ -0,0 +1,35 @@
+//
+//  Surf_EscapeTests.swift
+//  Surf EscapeTests
+//
+//  Created by Joshua Truscott on 30/06/2014.
+//  Copyright (c) 2014 JT App Development. All rights reserved.
+//
+
+import XCTest
+
+class Surf_EscapeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
